8c2906819a9407970933960020b83206d6a6f4b8
Lucas Santana <lanatnas36@gmail.com>
2020-08-01 13:34:39 -0300

    Primary commit

---
+++ build/precache-manifest.59366be78fb32f04f689122a225dbd19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f218df400f42da2fbf77a934b635d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a23e68173efa16a04a",
+    "url": "/static/css/main.39e56b30.chunk.css"
+  },
+  {
+    "revision": "9083e002d324f4f6cfd8",
+    "url": "/static/js/2.f5d2b9a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5d2b9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a23e68173efa16a04a",
+    "url": "/static/js/main.94f198d4.chunk.js"
+  },
+  {
+    "revision": "d4a5648972102e88a2a2",
+    "url": "/static/js/runtime-main.e1ab3c87.js"
+  },
+  {
+    "revision": "ecc0be876616227c0d0f75a62a3e155e",
+    "url": "/static/media/Logo.ecc0be87.png"
+  }
+]);
