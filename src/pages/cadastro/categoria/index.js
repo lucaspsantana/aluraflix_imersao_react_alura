24d60c3aaa8ae07b4cd8156f7c443327e883194d
Lucas Santana <lanatnas36@gmail.com>
2020-08-06 20:21:27 -0300

    insert state

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom'
 
 function CadastroCategoria(){
+  const [name, setValue] = useState("Filmes");
+ 
     return(
       <PageDefault >
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {name}</h1>
+        <form>
+          <label>
+            Nome da Categoria
+          
+          <input type="text" name="nome" value={name} onChange={ function setHandle(info){
+                  console.log(["[name]", name]);
+                  console.log('[info]',info.target.value);
+                  setValue(info.target.value);
+         }}/>
+          </label>
+          
+          <button>Cadastrar</button>
+        </form>
         <Link to="/">Ir pra Home</Link>
       </PageDefault>
     );
